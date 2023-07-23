31955eeafe1aa19a3293b7b748e2b3554e5a492d
ffonseca <ffonseca@jeracapital.com.br>
2023-07-23 19:55:12 -0300

    add exemple api rest

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+stores = [
+    {
+        "name": "myStore",
+        "itens": [
+            {
+                "name": "chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_store() -> dict[str, list[dict[{str, str}]]]:
+    return {"stores": stores}
