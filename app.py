a9b96600f7842afdb2844597da3f6d8ffca6630b
ffonseca <ffonseca@jeracapital.com.br>
2023-07-23 20:15:33 -0300

    add simple post api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,3 +17,12 @@ stores = [
 @app.get("/store")
 def get_store() -> dict[str, list[dict[{str, str}]]]:
     return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+
+    return new_store, 201
