22474a255daf0aa630bfedbd1fdf08fed4d6d94e
ffonseca <ffonseca@jeracapital.com.br>
2023-07-23 22:12:01 -0300

    add service (post) add item

--- app.py
+++ app.py
@@ -18,13 +18,11 @@ stores = [
 def get_store() -> dict[str, list[dict[{str, str}]]]:
     return {"stores": stores}
 
-
 @app.post("/store")
 def create_store():
     request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
+    new_store = {"name": request_data["name"], "itens": []}
     stores.append(new_store)
-
     return new_store, 201
 
 @app.post("/store/<string:name>/item")
@@ -36,3 +34,17 @@ def create_item(name: str):
             store["itens"].append(newitem)
             return newitem, 201
     return {"message": "store not found"}, 404
+
+@app.get("/store/<string:name>")
+def get_store_by_name(name):
+    for store in stores:
+        if (store["name"] == name):
+            return store
+    return { "message": "store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if (store["name"] == name):
+            return {"items": store["itens"]}
+    return { "message": "store not found"}, 404
