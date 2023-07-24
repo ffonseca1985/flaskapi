deb06eb9d0f315aac29bc056d74221f5604dc8b2
ffonseca <ffonseca@jeracapital.com.br>
2023-07-24 08:47:30 -0300

    add delete and update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from db import items, stores
+from flask_smorest import abort
 import uuid
 
 app = Flask(__name__)
@@ -16,30 +17,59 @@ def create_store():
     stores[store_id] = store
     return store, 201
 
-@app.post("/item")
-def create_item(name: str):
-    item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        return {"message": "store not found"}, 404
-
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-
-    return item, 201
-
-def get_all_items():
-    return { "items": list(items.values())}
-
 @app.get("/store/<string:store_id>")
-def get_store_by_name(store_id):
+def get_store_by_id(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return { "message": "store not found." }, 404
+         abort(404, message="store not found")
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted"}
+    except KeyError:
+         abort(404, message="store not found")
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        abort(404, message="store not found")
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+    return item, 201
+
+@app.get("/item")
+def get_all_items():
+    return { "items": list(items.values())}
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return { "message": "store not found"}, 404
+        abort(404, message="item not found")
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    try:
+        item = items[item_id]
+        item |= item_data #merge between dictionary
+
+        return item
+    except KeyError:
+        abort(404, message="item not found" )
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except KeyError:
+        abort(404, message="item not found")
